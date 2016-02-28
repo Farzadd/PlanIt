4761ef4a94648cab2866d4433f37bd1458cc5f7b
FarzF13 <farzadf13@gmail.com>
2016-02-27 20:04:45 -0800

    Fixed compile issues - reverted logic to be on android side.

--- Client/PlanIt/PlanIt.Droid/Activities/MainActivity.cs
+++ Client/PlanIt/PlanIt.Droid/Activities/MainActivity.cs
@@ -28,7 +28,53 @@ namespace PlanIt.Droid
             // Create the Mobile Service Client instance, using the provided
             // Mobile Service URL
             
-            PlanIt.Main.client = new MobileServiceClient(applicationURL);
+            PlanIt.Main.client = new MobileServiceClient(PlanIt.Main.applicationURL);
+        }
+
+        private async Task<bool> Authenticate()
+        {
+            var success = false;
+            try
+            {
+                // Sign in with Facebook login using a server-managed flow.
+                PlanIt.Main.user = await PlanIt.Main.client.LoginAsync(this,
+                    MobileServiceAuthenticationProvider.Facebook);
+
+                success = true;
+            }
+            catch (Exception ex)
+            {
+                CreateAndShowDialog(ex, "Authentication failed");
+            }
+            return success;
+        }
+
+        [Java.Interop.Export()]
+        public async void LoginUser(View view)
+        {
+            // Load data only after authentication succeeds.
+            if (await Authenticate())
+            {
+                //Hide the button after authentication succeeds.
+                FindViewById<Button>(Resource.Id.buttonLoginUser).Visibility = ViewStates.Gone;
+
+                // Load the data.
+                //OnRefreshItemsSelected();
+            }
+        }
+
+        private void CreateAndShowDialog(Exception exception, String title)
+        {
+            CreateAndShowDialog(exception.Message, title);
+        }
+
+        private void CreateAndShowDialog(string message, string title)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.SetMessage(message);
+            builder.SetTitle(title);
+            builder.Create().Show();
         }
     }
 }
--- Client/PlanIt/PlanIt/Main.cs
+++ Client/PlanIt/PlanIt/Main.cs
@@ -3,18 +3,15 @@ using System.Threading.Tasks;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Android.Views;
 
-using Microsoft.WindowsAzure.MobileServices;
-using System.Threading.Tasks;
-using System.IO;
+
 
 namespace PlanIt
 {
     public class Main
     {
         //Mobile Service Client reference
-        public MobileServiceClient client;
+        public static MobileServiceClient client;
 
         public const string applicationURL = @"https://planit-server.azurewebsites.net";
 
@@ -22,66 +19,21 @@ namespace PlanIt
             new MobileServiceClient(
             applicationURL
          );
+        public static MobileServiceUser user;
 
-        public MobileServiceUser user;
 
-        private async Task<bool> Authenticate()
-        {
-            var success = false;
-            try
-            {
-                // Sign in with Facebook login using a server-managed flow.
-                user = await client.LoginAsync(this,
-                    MobileServiceAuthenticationProvider.Facebook);
+        /*
+        User newUser = new User();
+        newUser.FacebookID = PlanIt.Main.user.UserId;
+        newUser.FacebookName = "CHEESECAKE";
 
-                User newUser = new User();
-                newUser.FacebookID = user.UserId;
-                newUser.FacebookName = "CHEESECAKE";
+        var result = await client
+            .InvokeApiAsync<User, string>("createUser", newUser);
 
-                var result = await client
-                    .InvokeApiAsync<User, string>("createUser", newUser);
+        CreateAndShowDialog(result, "FARZ");
 
-                CreateAndShowDialog(result, "FARZ");
-
-                CreateAndShowDialog(string.Format("you are now logged in - {0}",
-                    user.UserId), "Logged in!");
-
-                success = true;
-				SetContentView (Resource.Layout.Main);
-            }
-            catch (Exception ex)
-            {
-                CreateAndShowDialog(ex, "Authentication failed");
-            }
-            return success;
-        }
-
-        [Java.Interop.Export()]
-        public async void LoginUser(View view)
-        {
-            // Load data only after authentication succeeds.
-            if (await Authenticate())
-            {
-                //Hide the button after authentication succeeds.
-                FindViewById<Button>(Resource.Id.buttonLoginUser).Visibility = ViewStates.Gone;
-
-                // Load the data.
-                //OnRefreshItemsSelected();
-            }
-        }
-
-        private void CreateAndShowDialog(Exception exception, String title)
-        {
-            CreateAndShowDialog(exception.Message, title);
-        }
-
-        private void CreateAndShowDialog(string message, string title)
-        {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-            builder.SetMessage(message);
-            builder.SetTitle(title);
-            builder.Create().Show();
-        }
+        CreateAndShowDialog(string.Format("you are now logged in - {0}",
+            user.UserId), "Logged in!");
+        */
     }
 }
