6c1dd4fe847921a51779e7de604059bd97db2d29
Jun <cygoh@umich.edu>
2016-02-28 04:35:13 -0800

    radio buttons for attending status

--- Client/PlanIt/PlanIt.Droid/EventDetails.cs
+++ Client/PlanIt/PlanIt.Droid/EventDetails.cs
@@ -13,7 +13,7 @@ using Android.Widget;
 
 namespace PlanIt.Droid
 {
-	[Activity (Label = "EventDetails")]			
+	[Activity (Label = "EventDetails", MainLauncher = true)]			
 	public class EventDetails : Activity
 	{
 		protected override void OnCreate (Bundle savedInstanceState)
@@ -22,6 +22,20 @@ namespace PlanIt.Droid
 
 			// Create your application here
 			SetContentView (Resource.Layout.EventDetails);
+
+			RadioButton radio_going = FindViewById<RadioButton>(Resource.Id.radio_going);
+			RadioButton radio_maybe = FindViewById<RadioButton>(Resource.Id.radio_maybe);
+			RadioButton radio_cannot = FindViewById<RadioButton>(Resource.Id.radio_cannot);
+
+			radio_going.Click += RadioButtonClick;
+			radio_maybe.Click += RadioButtonClick;
+			radio_cannot.Click += RadioButtonClick;
+		}
+
+		private void RadioButtonClick (object sender, EventArgs e)
+		{
+			RadioButton rb = (RadioButton)sender;
+			Toast.MakeText (this, rb.Text, ToastLength.Short).Show ();
 		}
 	}
 }
